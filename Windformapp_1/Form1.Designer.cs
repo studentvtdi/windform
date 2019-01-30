b7a8cbdb12c21b0f05261b6f62aef2b1d4ed6eb5
VTDI <VTDI@DESKTOP-9JLO7D2>
2019-01-29 20:40:42 -0500

    intial commit

--- Windformapp_1/Form1.Designer.cs
+++ Windformapp_1/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My first windform";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter your name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(18, 118);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(241, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 66);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "BTMcomfirm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "My first windform";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Windformapp_1/Form1.cs
+++ Windformapp_1/Form1.cs
@@ -16,5 +16,15 @@ namespace Windformapp_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
